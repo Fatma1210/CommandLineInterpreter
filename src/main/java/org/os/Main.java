bf10518e6ee49f5e6d342cee5c67279a185b1c77
Fatmaa3 <119888493+Fatmaa3@users.noreply.github.com>
2024-10-28 10:53:41 +0300

    an added message to the user

--- src/main/java/org/os/Main.java
+++ src/main/java/org/os/Main.java
@@ -1,9 +1,11 @@
 package org.os;
 import org.CommandsFactory.commandsFactory;
 import java.util.Scanner;
+import java.io.IOException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello in our Command Line Intrepreter :) \n");
+        System.out.println("Type 'exit' to close the program or 'help' to show the list of commands.\n");
         Scanner scanner = new Scanner(System.in);
         commandsFactory Factory = new commandsFactory();
         while (true) {
